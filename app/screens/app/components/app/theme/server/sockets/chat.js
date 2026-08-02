7b5b9984a60bee397552b9a6935336599a6b9bc7
topone000659-blip <topone000659@gmail.com>
2026-08-02 16:26:30 +0630

    Implement socket connection and messaging functionality

--- app/screens/app/components/app/theme/server/sockets/chat.js
+++ app/screens/app/components/app/theme/server/sockets/chat.js
@@ -1,1 +1,64 @@
+module.exports = (io) => {
+
+
+  io.on("connection", (socket) => {
+
+
+    console.log(
+      "User connected:",
+      socket.id
+    );
+
+
+
+    socket.on(
+      "joinRoom",
+      (userId)=>{
+
+        socket.join(userId);
+
+        console.log(
+          "User joined:",
+          userId
+        );
+
+      }
+    );
+
+
+
+    socket.on(
+      "sendMessage",
+      (data)=>{
+
+
+        io.to(data.receiver_id)
+          .emit(
+            "receiveMessage",
+            data
+          );
+
+
+      }
+    );
+
+
+
+    socket.on(
+      "disconnect",
+      ()=>{
+
+        console.log(
+          "User disconnected:",
+          socket.id
+        );
+
+      }
+    );
+
+
+  });
+
+
+};
 
