aef094f290bbc73a8b5a4078e6cf73a3515ba2a1
topone000659-blip <topone000659@gmail.com>
2026-08-02 20:09:24 +0630

    Add success and error response utility functions

--- app/screens/app/components/app/theme/server/utils/response.js
+++ app/screens/app/components/app/theme/server/utils/response.js
@@ -1,1 +1,20 @@
+const success = (res, data = {}, message = "Success", status = 200) => {
+  return res.status(status).json({
+    success: true,
+    message,
+    data
+  });
+};
+
+const error = (res, message = "Error", status = 500) => {
+  return res.status(status).json({
+    success: false,
+    message
+  });
+};
+
+module.exports = {
+  success,
+  error
+};
 
