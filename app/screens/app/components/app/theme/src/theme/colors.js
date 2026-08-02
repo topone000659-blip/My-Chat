e4df50d8d7237df109c855e75db30e10deefb83e
topone000659-blip <topone000659@gmail.com>
2026-08-02 10:05:12 +0630

    Add color definitions for theme

--- app/screens/app/components/app/theme/src/theme/colors.js
+++ app/screens/app/components/app/theme/src/theme/colors.js
@@ -1,1 +1,23 @@
+const colors = {
+
+  primary: "#16C784",
+
+  background: "#FFFFFF",
+
+  text: "#111111",
+
+  secondaryText: "#777777",
+
+  bubbleSend: "#16C784",
+
+  bubbleReceive: "#EEEEEE",
+
+  border: "#DDDDDD",
+
+  danger: "#FF3333"
+
+};
+
+
+export default colors;
 
