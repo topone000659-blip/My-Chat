48101554d2705b91c4a21f0db4f71d3c8eb05ffb
topone000659-blip <topone000659@gmail.com>
2026-08-02 16:32:36 +0630

    Refactor server to use http and socket.io

--- app/screens/app/components/app/theme/server/src/server.js
+++ app/screens/app/components/app/theme/server/src/server.js
@@ -1,13 +1,42 @@
 require("dotenv").config();
 
+const http = require("http");
+
 const app = require("./app");
+
+const { Server } = require("socket.io");
+
+const chatSocket = require("./sockets/chat");
+
 
 const PORT = process.env.PORT || 3000;
 
 
-app.listen(PORT, () => {
 
-  console.log(`My Chat Server running on port ${PORT}`);
+const server = http.createServer(app);
+
+
+
+const io = new Server(server, {
+
+  cors: {
+
+    origin: "*"
+
+  }
 
 });
 
+
+
+chatSocket(io);
+
+
+
+server.listen(PORT, ()=>{
+
+  console.log(
+    `My Chat Server running on port ${PORT}`
+  );
+
+});
