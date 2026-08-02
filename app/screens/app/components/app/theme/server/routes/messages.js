4fd5559c883ae4ea7acc1c7b353f291e2bb4d6ca
topone000659-blip <topone000659@gmail.com>
2026-08-02 16:14:16 +0630

    Update messages.js

--- app/screens/app/components/app/theme/server/routes/messages.js
+++ app/screens/app/components/app/theme/server/routes/messages.js
@@ -1,1 +1,28 @@
+const express = require("express");
+
+const {
+  sendMessage,
+  getMessages
+} = require("../controllers/messageController");
+
+
+const router = express.Router();
+
+
+
+router.post(
+  "/",
+  sendMessage
+);
+
+
+
+router.get(
+  "/:user1/:user2",
+  getMessages
+);
+
+
+
+module.exports = router;
 
