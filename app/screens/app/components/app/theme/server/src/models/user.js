a1feaabd24ad0aaa5b163f5dae97ba91c05311d6
topone000659-blip <topone000659@gmail.com>
2026-08-02 15:31:43 +0630

    Update user.js

--- app/screens/app/components/app/theme/server/src/models/user.js
+++ app/screens/app/components/app/theme/server/src/models/user.js
@@ -1,1 +1,54 @@
+const pool = require("../config/database");
+
+
+const createUser = async (
+  username,
+  phone,
+  password
+) => {
+
+  const result = await pool.query(
+
+    `INSERT INTO users
+    (username, phone, password)
+    VALUES ($1,$2,$3)
+    RETURNING *`,
+
+    [
+      username,
+      phone,
+      password
+    ]
+
+  );
+
+
+  return result.rows[0];
+
+};
+
+
+
+const findUserByPhone = async (phone) => {
+
+  const result = await pool.query(
+
+    `SELECT * FROM users
+     WHERE phone=$1`,
+
+    [phone]
+
+  );
+
+
+  return result.rows[0];
+
+};
+
+
+
+module.exports = {
+  createUser,
+  findUserByPhone
+};
 
