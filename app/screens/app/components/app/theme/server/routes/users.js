585caddfd2f2dd898aebfe99d88611e27ac31cb7
topone000659-blip <topone000659@gmail.com>
2026-08-02 19:43:54 +0630

    Update users.js

--- app/screens/app/components/app/theme/server/routes/users.js
+++ app/screens/app/components/app/theme/server/routes/users.js
@@ -1,1 +1,28 @@
+const express = require("express");
+
+const auth = require("../middleware/auth");
+
+const {
+  getProfile,
+  updateProfile
+} = require("../controllers/userController");
+
+const router = express.Router();
+
+
+router.get(
+  "/profile",
+  auth,
+  getProfile
+);
+
+
+router.put(
+  "/profile",
+  auth,
+  updateProfile
+);
+
+
+module.exports = router;
 
