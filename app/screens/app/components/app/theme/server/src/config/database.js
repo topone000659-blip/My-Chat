e0eef5f48f82b345381c1c5dda6d92af83a8fd00
topone000659-blip <topone000659@gmail.com>
2026-08-02 15:22:44 +0630

    Set up PostgreSQL database connection pool

--- app/screens/app/components/app/theme/server/src/config/database.js
+++ app/screens/app/components/app/theme/server/src/config/database.js
@@ -1,1 +1,20 @@
+const { Pool } = require("pg");
+
+
+const pool = new Pool({
+
+  host: process.env.DATABASE_HOST,
+
+  port: process.env.DATABASE_PORT,
+
+  database: process.env.DATABASE_NAME,
+
+  user: process.env.DATABASE_USER,
+
+  password: process.env.DATABASE_PASSWORD
+
+});
+
+
+module.exports = pool;
 
