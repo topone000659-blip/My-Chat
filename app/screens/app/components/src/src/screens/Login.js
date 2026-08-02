50af57270f6e85069855e247caff72f157e733e5
topone000659-blip <topone000659@gmail.com>
2026-08-03 00:33:31 +0630

    Update Login.js

--- app/screens/app/components/src/src/screens/Login.js
+++ app/screens/app/components/src/src/screens/Login.js
@@ -1,136 +1,126 @@
-import React, {useState} from "react";
+import React,{useState} from "react";
 
 import {
-  View,
-  Text,
-  TextInput,
-  Button,
-  Alert
+ View,
+ Text,
+ TextInput,
+ Button,
+ Alert
 } from "react-native";
 
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 import API from "../services/api";
 
+import {saveUser} from "../services/storage";
 
 
-export default function Login({navigation}) {
 
+export default function Login({navigation}){
 
-  const [phone,setPhone] = useState("");
 
-  const [password,setPassword] = useState("");
+const [phone,setPhone]=useState("");
 
+const [password,setPassword]=useState("");
 
 
-  const login = async()=>{
 
-    try{
+const login=async()=>{
 
 
-      const response = await API.post(
+try{
 
-        "/auth/login",
 
-        {
-          phone,
-          password
-        }
+const response =
+await API.post(
+"/auth/login",
+{
+ phone,
+ password
+}
+);
 
-      );
 
 
+await AsyncStorage.setItem(
+"token",
+response.data.token
+);
 
-      const token = response.data.token;
 
 
+if(response.data.user){
 
-      await AsyncStorage.setItem(
+ await saveUser(
+  response.data.user
+ );
 
-        "token",
+}
 
-        token
 
-      );
 
+Alert.alert(
+"Success",
+"Login success"
+);
 
 
-      Alert.alert(
 
-        "Success",
+navigation.replace(
+"ChatList"
+);
 
-        "Login success"
 
-      );
 
+}catch(error){
 
 
-      navigation.replace("ChatList");
+Alert.alert(
+"Error",
+"Login failed"
+);
 
 
+}
 
-    }catch(error){
 
+};
 
-      Alert.alert(
 
-        "Error",
 
-        "Login failed"
+return(
 
-      );
+<View>
 
+<Text>
+Login
+</Text>
 
-    }
 
-  };
+<TextInput
+placeholder="Phone"
+value={phone}
+onChangeText={setPhone}
+/>
 
 
+<TextInput
+placeholder="Password"
+secureTextEntry
+value={password}
+onChangeText={setPassword}
+/>
 
-  return (
 
-    <View>
+<Button
+title="Login"
+onPress={login}
+/>
 
-      <Text>
-        Login
-      </Text>
 
+</View>
 
-      <TextInput
+);
 
-        placeholder="Phone"
-
-        value={phone}
-
-        onChangeText={setPhone}
-
-      />
-
-
-      <TextInput
-
-        placeholder="Password"
-
-        secureTextEntry
-
-        value={password}
-
-        onChangeText={setPassword}
-
-      />
-
-
-      <Button
-
-        title="Login"
-
-        onPress={login}
-
-      />
-
-
-    </View>
-
-  );
 
 }
