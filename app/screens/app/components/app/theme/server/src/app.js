d36e551aa7ec2d5c7c87283dd743351950cf60a9
topone000659-blip <topone000659@gmail.com>
2026-08-02 15:14:41 +0630

    Set up Express server with CORS and Helmet

--- app/screens/app/components/app/theme/server/src/app.js
+++ app/screens/app/components/app/theme/server/src/app.js
@@ -1,1 +1,28 @@
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+
+const app = express();
+
+
+app.use(helmet());
+
+app.use(cors());
+
+app.use(express.json());
+
+
+app.get("/", (req, res) => {
+
+  res.json({
+    message: "My Chat Server is running"
+  });
+
+});
+
+
+module.exports = app;
+
+
+
 
