3f590e465b1e12eddf0c288b7f3a5ebb0f539e4b
topone000659-blip <topone000659@gmail.com>
2026-08-02 16:46:36 +0630

    Update chatController.js

--- app/screens/app/components/app/theme/server/chatController.js
+++ app/screens/app/components/app/theme/server/chatController.js
@@ -1,1 +1,54 @@
+const pool = require("../config/database");
+
+
+
+const getChatList = async (req,res)=>{
+
+  try{
+
+    const userId = req.user.id;
+
+
+    const result = await pool.query(
+
+      `
+      SELECT DISTINCT
+      CASE
+        WHEN sender_id=$1 THEN receiver_id
+        ELSE sender_id
+      END AS user_id
+
+      FROM messages
+
+      WHERE sender_id=$1
+      OR receiver_id=$1
+      `,
+
+      [userId]
+
+    );
+
+
+    res.json(result.rows);
+
+
+  }catch(error){
+
+    res.status(500).json({
+
+      message:error.message
+
+    });
+
+  }
+
+};
+
+
+
+module.exports = {
+
+  getChatList
+
+};
 
