4f70b5ebf4c135aeb0139f47ebc8399e67428f43
topone000659-blip <topone000659@gmail.com>
2026-08-03 00:38:31 +0630

    Update storage.js

--- app/screens/app/components/src/src/services/storage.js
+++ app/screens/app/components/src/src/services/storage.js
@@ -1,1 +1,24 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
+
+export const saveUser = async(user)=>{
+
+  await AsyncStorage.setItem(
+    "user",
+    JSON.stringify(user)
+  );
+
+};
+
+
+export const getUser = async()=>{
+
+  const user =
+    await AsyncStorage.getItem("user");
+
+
+  return user
+    ? JSON.parse(user)
+    : null;
+
+};
