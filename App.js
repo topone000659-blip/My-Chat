ff965332917f0e191f7c34a9f2ff4ee8131424dd
topone000659-blip <topone000659@gmail.com>
2026-08-02 19:48:21 +0630

    Update App.js

--- App.js
+++ App.js
@@ -7,69 +7,42 @@ const pool = require("./config/database");
 const authRoutes = require("./routes/auth");
 const messageRoutes = require("./routes/messages");
 const chatRoutes = require("./routes/chats");
-
+const userRoutes = require("./routes/users");
 
 const app = express();
 
-
 app.use(helmet());
-
 app.use(cors());
-
 app.use(express.json());
 
-
-
 app.use("/auth", authRoutes);
-
 app.use("/messages", messageRoutes);
-
 app.use("/chats", chatRoutes);
+app.use("/users", userRoutes);
 
-
-
-app.get("/", (req,res)=>{
-
+app.get("/", (req, res) => {
   res.json({
-    message:"My Chat Server is running"
+    message: "My Chat Server is running"
   });
-
 });
 
-
-
-app.get("/database-test", async(req,res)=>{
-
-  try{
-
-    const result = await pool.query(
-      "SELECT NOW()"
-    );
-
+app.get("/database-test", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT NOW()");
 
     res.json({
-
-      database:"connected",
-
-      time:result.rows[0]
-
+      database: "connected",
+      time: result.rows[0]
     });
 
-
-  }catch(error){
+  } catch (error) {
 
     res.status(500).json({
-
-      database:"error",
-
-      message:error.message
-
+      database: "error",
+      message: error.message
     });
 
   }
-
 });
-
-
 
 module.exports = app;
