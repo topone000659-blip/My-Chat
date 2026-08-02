aae6bc350374d523d2cf0844e7cb0a6bf2734e90
topone000659-blip <topone000659@gmail.com>
2026-08-02 16:41:57 +0630

    Update messages.js

--- app/screens/app/components/app/theme/server/routes/messages.js
+++ app/screens/app/components/app/theme/server/routes/messages.js
@@ -1,5 +1,7 @@
 const express = require("express");
 
+const auth = require("../middleware/auth");
+
 const {
   sendMessage,
   getMessages
@@ -12,6 +14,7 @@ const router = express.Router();
 
 router.post(
   "/",
+  auth,
   sendMessage
 );
 
@@ -19,10 +22,10 @@ router.post(
 
 router.get(
   "/:user1/:user2",
+  auth,
   getMessages
 );
 
 
 
 module.exports = router;
-
