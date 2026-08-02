e8672b845576c375a25f835b17a608f8c516ec18
topone000659-blip <topone000659@gmail.com>
2026-08-02 15:09:28 +0630

    Update server.js

--- app/screens/app/components/app/theme/server/src/server.js
+++ app/screens/app/components/app/theme/server/src/server.js
@@ -1,1 +1,13 @@
+require("dotenv").config();
+
+const app = require("./app");
+
+const PORT = process.env.PORT || 3000;
+
+
+app.listen(PORT, () => {
+
+  console.log(`My Chat Server running on port ${PORT}`);
+
+});
 
