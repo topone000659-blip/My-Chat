7950e7af4e8a9faefe221fbb6eaac80be7093f13
topone000659-blip <topone000659@gmail.com>
2026-08-02 16:55:12 +0630

    Update chats.js

--- app/screens/app/components/app/theme/server/routes/chats.js
+++ app/screens/app/components/app/theme/server/routes/chats.js
@@ -1,1 +1,26 @@
+const express = require("express");
+
+const auth = require("../middleware/auth");
+
+const {
+  getChatList
+} = require("../controllers/chatController");
+
+
+const router = express.Router();
+
+
+
+router.get(
+  "/",
+  auth,
+  getChatList
+);
+
+
+
+module.exports = router;
+
+
+
 
